f4333c6550119c0563fa1ff88b48861034d95433
Jello-fish <joel.markussen@hotmail.com>
2026-02-13 22:41:32 +0100

    første commit med øvingsfiler

---
+++ test.py
@@ -0,0 +1,16 @@
+import sys
+
+#print("Hello, world!")
+#print(sys.version)
+
+#if 5 > 2:
+    #print ("Fem er størst")
+
+
+tall = 11
+
+if tall > 10:
+    print("Større enn 10")
+else:
+    print("Tall er mindre enn 10")
+
