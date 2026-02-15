440d0b0f07433007974ffa6c8338cb87f3405f8b
Jello-fish <joel.markussen@hotmail.com>
2026-02-16 00:06:27 +0100

    Oppgaver om variabler
    Gjorde et par ekstra oppgaver om variabler i python.

---
+++ python_variables/variabler.py
@@ -0,0 +1,13 @@
+#variabler i python må ikke definere dataype
+#kan ikke hete keywords eller tall osv.
+x = 1
+y = 2
+z = 3
+
+#Kan også definere variabler på en linje
+a = b = c = "epler"
+
+#printer ut alle tre variabler (funker også med ulike datatyper)
+print (x, y, z)
+#Operatører (x - osv.) legger sammen alle variabler (funker ikke med ulike datatyper)
+print (x + y + z)
