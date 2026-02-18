1df5878473cb42905c56fb8414f521388192612c
Jello-fish <joel.markussen@hotmail.com>
2026-02-18 23:58:28 +0100

    Variables done
    Finnished practicing basic variables in Python.

--- python_variables/variabler.py
+++ python_variables/variabler.py
@@ -11,3 +11,40 @@ a = b = c = "epler"
 print (x, y, z)
 #Operatører (x - osv.) legger sammen alle variabler (funker ikke med ulike datatyper)
 print (x + y + z)
+
+#Globale variabler, variabler utenfor en funksjon.
+variabel = "supert"
+def minfunksjon():
+    print ("python er " + variabel)
+minfunksjon()
+
+#Samme men inni funksjon, så er den LOKAL
+variabel = "supert"
+def minfunksjon():
+    variabel = "ekstra supert"
+    print ("python er " + variabel)
+minfunksjon()
+
+#man kan også lage en global variabel inni en funksjon
+def funksjon2():
+   global myvar 
+   myvar = 5
+funksjon2()
+
+#variabel er definert med nøkkelordet global inne i funksjon, men kan påkalles utenfor
+print (myvar)
+
+#Dette kan også endres inn i en funksjon
+#Variabel definert utenfor funksjon
+testfunc = 123
+#local variabel gjort global inne i funksjon
+def funksjon3():
+   global testfunc 
+   testfunc = 321
+funksjon3()
+#påkaller variabel utenfor funksjon, men tar den globale verdien
+print(testfunc)
+
+#Man kan også printe datatypen på en variabel med 'type()' funksjonen
+dataVar = "9"
+print(type(dataVar))
