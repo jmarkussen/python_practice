dd58c829d538553398a50f70071dd55fc47994d3
Jello-fish <joel.markussen@hotmail.com>
2026-03-08 19:53:15 +0100

    String formatting
    More tasks on string formatting added to the document

---
+++ datatypes/strings/concatenation.py
@@ -0,0 +1,12 @@
+#concatenation means combining
+#to combine two strings, simpy use the + operator
+
+a = "hello"
+b = "world"
+c = a + b
+print (c)
+
+#if you want a space between, just add a space
+
+d = a + " " + b
+print (d)
